26049cd1847a819c90f6af3788deabf058fea1a9
Pedro Ramos <pedroramos.contato@outlook.com>
2020-02-15 16:03:15 -0300

    :construction: Add offer acceptance controller

---
+++ backend/src/controllers/AcceptanceController.js
@@ -0,0 +1,39 @@
+const User = require('../models/User')
+const Offer = require('../models/Offer')
+
+module.exports = {
+  async store (req, res) {
+    try {
+      const userId = req.user_id
+      const { offerId } = req.params
+
+      const loggedUser = await User.findById(userId)
+
+      if (loggedUser.tipo !== 'Pessoa jurídica') {
+        return res
+          .status(401)
+          .json({ message: 'Você não têm autorização para isso' })
+      }
+
+      const offer = await Offer.findById(offerId)
+
+      offer.estado = 'Aceito'
+      offer.recebido = false
+      offer.recebedor = loggedUser._id
+      await offer.save()
+
+      // TODO: emitir uma mensagem pelo socket.io
+
+      await offer
+        .populate('recebedor')
+        .populate('doacao')
+        .execPopulate()
+
+      return res.status(201).json(offer)
+    } catch (err) {
+      return res
+        .status(500)
+        .send(err.toString() || err.message)
+    }
+  }
+}
--- backend/src/routes/offer.routes.js
+++ backend/src/routes/offer.routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 
 const OfferController = require('../controllers/OfferController')
+const AcceptanceController = require('../controllers/AcceptanceController')
 
 router.get('/', OfferController.index)
 router.post('/:donationId', OfferController.store)
+
+router.post('/:offerId/acceptance', AcceptanceController.store)
 
 module.exports = router
