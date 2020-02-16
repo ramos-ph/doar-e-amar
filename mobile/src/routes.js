72d2239389400e548b6be95c715669bf1103cd8d
Pedro Ramos <pedroramos.contato@outlook.com>
2020-02-16 16:28:36 -0300

    :construction: Add offer related navigator

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,13 +2,15 @@ import {createSwitchNavigator ,createAppContainer} from 'react-navigation';
 
 import AppNavigator from './navigators/app-navigator';
 import AuthNavigator from './navigators/auth-navigator';
+import OfferNavigator from './navigators/offer-navigator';
 import MapNavigator from './navigators/map-navigator';
 
 const RootNavigator = createSwitchNavigator(
   {
     Auth: AuthNavigator,
     App: AppNavigator,
-    Map: MapNavigator
+    Map: MapNavigator,
+    New: OfferNavigator
   },
   {
     initialRouteName: 'Auth'
