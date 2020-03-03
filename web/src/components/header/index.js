d10636047b2b57df74f3193d585770a8d56a8bbd
Pedro Ramos <pedroramos.contato@outlook.com>
2020-03-03 19:52:01 -0300

    :lipstick: Update header style

--- web/src/components/header/index.js
+++ web/src/components/header/index.js
@@ -3,21 +3,23 @@ import React from 'react'
 import './styles.css'
 
 import logo from '../../assets/img/doar-e-amar.png'
+import next from '../../assets/img/icons/next.svg'
 
 export default function Header () {
   return (
-    <header>
+    <header id="header-home">
       <div className="navbar">
         <img src={logo} alt="Logo" title="Doar e Amar" />
 
         <nav role="navigation">
           <a className="nav-link" href="#painel">Início</a>
-          <a className="nav-link" href="#doar">Notícias</a>
-          <a className="nav-link" href="#sobre">Sobre nós</a>
-          <a className="nav-link" href="#parceiros">Parceiros</a>
-          <a className="nav-link" href="#fale-conosco">Fale Conosco</a>
+          <a className="nav-link" href="#sobre">Sobre</a>
+          <a className="nav-link" href="#parceiros">Quem somos</a>
+          <a className="nav-link" href="#painel">Blog</a>
 
-          <button className="header-btn">Participar</button>
+          <button className="header-btn">Participe
+            <img src={next} alt=">" />
+          </button>
         </nav>
       </div>
     </header>
