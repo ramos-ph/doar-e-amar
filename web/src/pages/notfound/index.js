ef84c133ad4dde9cbb13cb2b00eff9ffe48e6678
Pedro Ramos <pedroramos.contato@outlook.com>
2020-03-06 01:29:18 -0300

    :sparkles: Add Not Found page

---
+++ web/src/pages/notfound/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './styles.css'
+
+import Header from '../../components/header'
+
+export default function NotFound () {
+  return (
+    <div className="not-found-container">
+      <Header />
+
+      <div className="wrapper not-found-content m-wrap">
+        <h1>Ops..</h1>
+        <h3>Parece que não há nada por aqui.</h3>
+
+        <Link to="/">Voltar ao início</Link>
+      </div>
+    </div>
+  )
+}
--- web/src/routes.js
+++ web/src/routes.js
@@ -3,12 +3,15 @@ import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/main'
 import LoginPage from './pages/login'
+import NotFoundPage from './pages/notfound'
 
 const MainRouter = () => (
   <Routes>
     <Route path="/" element={<MainPage />} />
 
     <Route path="/signin" element={<LoginPage />} />
+
+    <Route path="*" element={<NotFoundPage />} />
   </Routes>
 )
 
