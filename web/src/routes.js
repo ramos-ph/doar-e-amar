b9d02a9d7a7296a800c071cb8bb616da8b2d1af4
Pedro Ramos <pedroramos.contato@outlook.com>
2020-04-25 15:27:22 -0300

    :truck: Rename pages to camel case

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import MainPage from './pages/main'
-import LoginPage from './pages/login'
-import NotFoundPage from './pages/notfound'
+import MainPage from './pages/Main'
+import NotFoundPage from './pages/NotFound'
 
 const MainRouter = () => (
   <Routes>
     <Route path="/" element={<MainPage />} />
-
-    <Route path="/signin" element={<LoginPage />} />
 
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
