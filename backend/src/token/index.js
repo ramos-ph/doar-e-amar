4297d91f6233c7652f9b61dac5b2042666a5fe33
Pedro Ramos <pedroramos.contato@outlook.com>
2020-04-28 14:35:41 -0300

    :fire: Delete token folder

--- backend/src/token/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authorization = require('./authorizateToken');
-
-router.use(authorization);
-
-router.get('/auth', (req, res) => res.status(200).send({ authorized: true, user: req.user_id }));
-
-module.exports = router;
