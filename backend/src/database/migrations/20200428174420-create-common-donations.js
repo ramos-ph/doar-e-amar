b72608eb57a6ce16b435d4cc6bdcb9fa6b607b0b
Pedro Ramos <pedroramos.contato@outlook.com>
2020-04-28 14:47:37 -0300

    :card_file_box: Add common donations migration

---
+++ backend/src/database/migrations/20200428174420-create-common-donations.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('common_donations', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    picture: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    pickupAddress: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: Sequelize.DATE,
+    updated_at: Sequelize.DATE,
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('common_donations'),
+};
