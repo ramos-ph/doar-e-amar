7f0c176b13a7137ce5eb418cf5cabb3cf1c9cf3e
Pedro Ramos <pedroramos.contato@outlook.com>
2020-04-28 14:42:31 -0300

    :card_file_box: Add categories migration

--- backend/src/database/migrations/20200428174133-create-categories.js
+++ backend/src/database/migrations/20200428174133-create-categories.js
@@ -1,17 +1,17 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('donations', {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('categories', {
     id: {
       type: Sequelize.INTEGER,
       primaryKey: true,
       allowNull: false,
       autoIncrement: true,
     },
-    title: {
+    name: {
       type: Sequelize.STRING,
       allowNull: false,
     },
   }),
 
   // eslint-disable-next-line no-unused-vars
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('donations'),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('categories'),
 };
