c3b164b93b160956c5e8a7e8cf084528e287f0cc
Pedro Ramos <pedroramos.contato@outlook.com>
2020-05-18 21:18:30 -0300

    :sparkles: Add offer controller

--- backend/src/controllers/OfferController.js
+++ backend/src/controllers/OfferController.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const Donation = require('../models/Donation');
 const CommonDonation = require('../models/CommonDonation');
 const Donator = require('../models/Donator');
@@ -5,11 +7,60 @@ const Category = require('../models/Category');
 
 module.exports = {
   async index(req, res, next) {
+    const { category } = req.query;
+
     try {
       const donations = await Donation.findAll({
         where: {
           monetary_donation_id: null,
         },
+        include: [{
+          model: CommonDonation,
+          as: 'common_donation',
+          where: {
+            status: 'PENDENTE',
+          },
+        }, {
+          model: Donator,
+          as: 'donator',
+          attributes: {
+            exclude: [
+              'password',
+              'cpf',
+              'address',
+              'member',
+              'contact_id',
+            ],
+          },
+        }, {
+          model: Category,
+          as: 'category',
+          where: category ? {
+            name: {
+              [Op.iLike]: `%${category}%`,
+            },
+          } : {},
+        }],
+        attributes: {
+          exclude: [
+            'donator_id',
+            'common_donation_id',
+            'category_id',
+          ],
+        },
+      });
+
+      return res.status(200).send(donations);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async show(req, res, next) {
+    const { offer_id } = req.params;
+
+    try {
+      const donation = await Donation.findByPk(offer_id, {
         include: [{
           model: CommonDonation,
           as: 'common_donation',
@@ -41,7 +92,17 @@ module.exports = {
         },
       });
 
-      return res.status(200).send(donations);
+      if (!donation) {
+        return next({
+          status: 404,
+          error: 'Donation not found.',
+          details: {
+            offer: `The offer with id ${offer_id} does not exists`,
+          },
+        });
+      }
+
+      return res.status(200).send(donation);
     } catch (err) {
       return next(err);
     }
--- backend/src/routes/offer.js
+++ backend/src/routes/offer.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const OfferController = require('../controllers/OfferController');
 
 router.get('/', OfferController.index);
+router.get('/:offer_id', OfferController.show);
 
 module.exports = router;
