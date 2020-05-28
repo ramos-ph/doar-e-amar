616a9d24d1f2ab6beacdf75547155b63650db926
Pedro Ramos <pedroramos.contato@outlook.com>
2020-05-28 12:19:04 -0300

    :construction: Add screens

--- doar_e_amar/src/routes/auth.routes.js
+++ doar_e_amar/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Main from '../pages/Main';
@@ -12,12 +12,52 @@ function AuthRoutes() {
       initialRouteName="Main"
       tabBarOptions={{activeTintColor: '#3498db'}}>
       <Tab.Screen
+        name="Donations"
+        component={Main}
+        options={{
+          title: 'Doações',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="view-list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Donate"
+        component={Main}
+        options={{
+          title: 'Nova doação',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="favorite" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
         name="Main"
         component={Main}
         options={{
           title: 'Início',
           tabBarIcon: ({color, size}) => (
             <Icon name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Main}
+        options={{
+          title: 'Buscar',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Main}
+        options={{
+          title: 'Perfil',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="account-circle" size={size} color={color} />
           ),
         }}
       />
