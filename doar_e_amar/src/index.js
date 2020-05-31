bcec0a1ee87082bb4304ab38aa8095969013a210
Pedro Ramos <pedroramos.contato@outlook.com>
2020-05-30 21:48:35 -0300

    :poop: Remove socket.io connection
    Bad code. Fix later.

--- doar_e_amar/src/index.js
+++ doar_e_amar/src/index.js
@@ -1,8 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {YellowBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Routes from './routes';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket', 'Failed child context']);
 
 function App() {
   return (
