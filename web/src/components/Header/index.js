0510edf936c98e23c5da6ee674bbf60eafb665c2
Pedro Ramos <pedroramos.contato@outlook.com>
2020-05-31 09:40:58 -0300

    :lipstick: Update header styles

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import { FiArrowRight } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
@@ -6,28 +7,38 @@ import './styles.css'
 
 import logo from '../../assets/img/logo.png'
 
-export default function Header ({ setIsModalShown }) {
+export default function Header ({ setIsModalShown, isLoggedIn }) {
   return (
     <header id="header-home">
       <div className="navbar">
         <img src={logo} alt="Logo" title="Doar e Amar" />
 
         <nav role="navigation">
-          <Link className="nav-link" to="/">Início</Link>
-          <Link className="nav-link" to="/">Sobre</Link>
-          <Link className="nav-link" to="/">Dúvidas frequentes</Link>
-          <button
-            onClick={() => setIsModalShown(true)}
-            className="nav-link"
-            style={{
-              background: 'none',
-              border: 'none'
-            }}>Login</button>
-
-          <Link className="header-btn" to="/">Participe
-            <FiArrowRight size={17} color="#FFF" />
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link className="nav-link" to="/">Minhas doações</Link>
+              <Link className="nav-link" to="/">Nova doação</Link>
+              <Link className="nav-link" to="/">Meu perfil</Link>
+            </>
+          ) : (
+            <>
+              <Link className="nav-link" to="/">Início</Link>
+              <Link className="nav-link" to="/">Sobre</Link>
+              <Link className="nav-link" to="/">Dúvidas frequentes</Link>
+              <button
+                onClick={() => setIsModalShown(true)}
+                className="nav-link"
+                style={{
+                  background: 'none',
+                  border: 'none'
+                }}>Login</button>
+            </>
+          )}
         </nav>
+
+        <Link className="header-btn" to="/">{isLoggedIn ? 'Sair' : 'Participe'}
+          <FiArrowRight size={17} color="#FFF" />
+        </Link>
       </div>
     </header>
   )
--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { FiSearch, FiGrid, FiHeart, FiSettings, FiPower, FiMenu } from 'react-icons/fi'
-import './styles.css'
+import { FiSearch } from 'react-icons/fi'
 
+import './styles.css'
+import Header from '../../components/Header'
 import api from '../../services/api'
 
 function Dashboard () {
   const [donations, setDonations] = useState([])
-  const [isMenuShown, setIsMenuShown] = useState(true)
 
   const user = JSON.parse(localStorage.getItem('data'))
   const userId = localStorage.getItem('user_id')
@@ -24,7 +24,7 @@ function Dashboard () {
       } catch (err) {
         const { response = err } = err
 
-        return alert(response.status === 500
+        return console.log(response.status === 500
           ? 'Ocorreu um erro interno. Por favor tente novamente'
           : 'Nenhuma doação encontrada.')
       }
@@ -34,78 +34,53 @@ function Dashboard () {
   }, [userId])
 
   return (
-    <div className="dashboard-container">
-      <aside className={isMenuShown ? 'active' : undefined}>
-        <div className="menu-toggler">
-          <strong>Doar e amar</strong>
+    <>
+      <Header isLoggedIn={true} />
 
-          <FiMenu size={28} color="#000" onClick={() => setIsMenuShown(!isMenuShown)}/>
+      <div className="dashboard-container">
+        <aside>
+          {/* magic */}
+        </aside>
+
+        <div className="content">
+          <header>
+            <div className="input-container">
+              <input type="text" placeholder="O que está procurando?" />
+
+              <button>
+                <FiSearch size={22} color="#000" />
+              </button>
+            </div>
+
+            <div className="header-content">
+              <img src={`http://localhost:3001/public/uploads/${user.avatar}`} alt={user.name} className="avatar"/>
+
+              <strong>{user.name}</strong>
+            </div>
+          </header>
+
+          <h2>Minhas doações</h2>
+          <ul className="donations-list">
+            {donations.map((donation) => (
+              <li key={donation.id}>
+                <img src={`http://localhost:3001/public/uploads/${donation.common_donation.picture}`} alt={donation.title}/>
+
+                <footer>
+                  <strong>{donation.title}</strong>
+                  <p className="address">{donation.common_donation.pickup_address}</p>
+
+                  <div className="status">
+                    <p>{donation.category.name}</p>
+
+                    <p>{donation.common_donation.status}</p>
+                  </div>
+                </footer>
+              </li>
+            ))}
+          </ul>
         </div>
-
-        <ul>
-          <li>
-            <FiGrid size={24} color="#6c5ce7" />
-            <a href="#">Minhas doações</a>
-          </li>
-          <li>
-            <FiHeart size={24} color="#EA2027" />
-            <a href="#">Nova doação</a>
-          </li>
-          {user.cnpj !== null && (
-            <li>
-              <FiSearch size={24} color="#009432" />
-              <a href="#">Ver ofertas</a>
-            </li>
-          )}
-          <li>
-            <FiSettings size={24} color="#12CBC4" />
-            <a href="#">Configurações</a>
-          </li>
-          <li>
-            <FiPower size={24} color="#F79F1F" />
-            <a href="#">Sair</a>
-          </li>
-        </ul>
-      </aside>
-
-      <div className="content">
-        <header>
-          <div className="input-container">
-            <input type="text" placeholder="O que está procurando?" />
-
-            <button>
-              <FiSearch size={22} color="#000" />
-            </button>
-          </div>
-
-          <div className="header-content">
-            <img src={`http://localhost:3001/public/uploads/${user.avatar}`} alt={user.name} className="avatar"/>
-
-            <strong>{user.name}</strong>
-          </div>
-        </header>
-
-        <h2>Minhas doações</h2>
-        <ul className="donations-list">
-          {donations.map((donation) => (
-            <li key={donation.id}>
-              <img src={`http://localhost:3001/public/uploads/${donation.common_donation.picture}`} alt={donation.title}/>
-
-              <footer>
-                <strong>{donation.title}</strong>
-                <p className="address">{donation.common_donation.pickup_address}</p>
-
-                <div className="status">
-                  <p>{donation.category.name}</p>
-
-                  <p>{donation.common_donation.status}</p>
-                </div>
-              </footer>
-            </li>
-          ))}
-        </ul>
       </div>
-    </div>
+    </>
   )
 }
 
