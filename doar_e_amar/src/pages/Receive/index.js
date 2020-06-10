013b03c3fe1eec069b9138418288726f0c07c361
Pedro Ramos <pedroramos.contato@outlook.com>
2020-06-10 14:14:48 -0300

    Add QRCode reader for receivement

--- doar_e_amar/src/pages/Receive/index.js
+++ doar_e_amar/src/pages/Receive/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import AsyncStorage from '';
+import {Text, Alert} from 'react-native';
 
 import styles from './styles';
+import api from '../../services/api';
 
 function Receive() {
+  async function onSuccess(e) {
+    try {
+      const storagedUser = await AsyncStorage.getItem('user');
+
+      const {id} = JSON.parse(storagedUser);
+
+      await api.put(`/donations/${e.data}/actions/receive`, null, {
+        headers: {
+          authorization: id,
+        },
+      });
+
+      Alert.alert(
+        'Oba!',
+        'Agradecemos por usar nossa plataforma! Faça bom uso.',
+      );
+    } catch (err) {
+      const {response = err} = err;
+
+      return Alert.alert(
+        'Ops..',
+        response.status === 403
+          ? 'Você não tem permissão para receber essa oferta.'
+          : 'Ocorreu um erro interno. Por favor, tente novamente',
+      );
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Receber</Text>
-    </View>
+    <QRCodeScanner
+      onRead={onSuccess}
+      style={styles.container}
+      topContent={
+        <Text style={styles.label}>
+          Escaneie o QRCode da doação para confirmar o recebimento
+        </Text>
+      }
+    />
   );
 }
 
--- doar_e_amar/src/pages/Receive/styles.js
+++ doar_e_amar/src/pages/Receive/styles.js
@@ -4,4 +4,11 @@ import {common} from '../../styles';
 
 export default StyleSheet.create({
   ...common,
+
+  label: {
+    width: '85%',
+    textAlign: 'center',
+    marginBottom: 30,
+    fontSize: 18,
+  },
 });
