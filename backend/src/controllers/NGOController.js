0b9ba0bd33a4ff75f3fb11f2346969cb9315ba7e
Pedro Ramos <pedroramos.contato@outlook.com>
2020-07-16 22:05:38 -0300

    Add email sender on NGO signup

--- backend/src/controllers/NGOController.js
+++ backend/src/controllers/NGOController.js
@@ -1,9 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 const Donator = require('../models/Donator');
 const DonatorContact = require('../models/DonatorContact');
 const validateEmail = require('../utils/validateEmail');
 
 const NGO = require('../services/ngo-service');
+
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
+});
 
 module.exports = {
   async index(_req, res, next) {
@@ -51,6 +56,30 @@ module.exports = {
         cnpj,
         contact_id: contact.id,
       });
+
+      const hashedId = bcrypt.hashSync(String(ngo.id), 6);
+
+      // To refactor.
+      const message = {
+        from: 'contato@doareamar.com',
+        to: ngo.email,
+        subject: 'Confirme seu e-mail na Doar & Amar',
+        html: `<p>Olá, ${ngo.name}!</p>
+        <br/>
+        Por favor, acesse o link abaixo para confirmar seu e-mail.
+        <br/><br/>
+        <a href="http://localhost:3001/api/doareamar/v1/confirm/?token=${hashedId}&?id=${ngo.id}">Acesse aqui.</a>`,
+      };
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_ADDRESS,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      transporter.sendMail(message);
 
       return res.status(201).send(ngo);
     } catch (err) {
