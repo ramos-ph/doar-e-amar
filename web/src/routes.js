3d05627f075ef36b01a07512a7255ca5940e34fb
Pedro Ramos <pedroramos.contato@outlook.com>
2020-07-16 23:00:07 -0300

    Delete useless page

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom'
 import MainPage from './pages/Main'
 import SignupPage from './pages/Signup'
 import DashboardPage from './pages/Dashboard'
-import DonationPage from './pages/Donation'
 import NewPage from './pages/New'
 import NotFoundPage from './pages/NotFound'
 import SigninPage from './pages/Signin'
@@ -15,7 +14,6 @@ const MainRouter = () => (
     <Route path="/signin" component={SigninPage} />
     <Route path="/signup" component={SignupPage} />
     <Route path="/dashboard" component={DashboardPage} />
-    <Route path="/donations/:donation_id" component={DonationPage} />
     <Route path="/new" component={NewPage} />
 
     <Route path="*" component={NotFoundPage} />
